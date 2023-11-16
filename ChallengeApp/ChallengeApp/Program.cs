8218544e4ac626cea7252e581d18252bd3f5a07e
Artur.Górski <artur.gorski@agar.pl>
2023-11-16 11:58:16 +0100

    Dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Dzień 4
+string name = "Ewa";
+string gender = "female";
+var age = 34;
+//
+if (gender == "female" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (gender == "male" && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else
+{
+    Console.WriteLine("Żaden warunek nie został spełniony");
+}
